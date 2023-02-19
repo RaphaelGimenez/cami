b12aa30b3854b0e60f01b9a1dcb58917ea06d14c
Raphael <contact@raphaelgimenez.fr>
2023-02-19 15:11:03 +0000

    feat: display clustered markers

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,21 +25,51 @@ export default function Home({ data }: Props) {
     });
 
     map.current.on("load", () => {
-      map.current?.addSource("earthquakes", {
+      map.current?.addSource("distributeurs", {
         type: "geojson",
         // Use a URL for the value for the `data` property.
         data,
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
       });
 
       map.current?.addLayer({
         id: "earthquakes-layer",
         type: "circle",
-        source: "earthquakes",
+        source: "distributeurs",
+        filter: ["has", "point_count"],
         paint: {
-          "circle-radius": 4,
-          "circle-stroke-width": 2,
-          "circle-color": "red",
+          "circle-color": "hsla(0,0%,0%,0.75)",
+          "circle-stroke-width": 1.5,
           "circle-stroke-color": "white",
+
+          "circle-radius": ["case", ["get", "cluster"], 10, 5],
+        },
+      });
+
+      map.current?.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "distributeurs",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": ["get", "point_count_abbreviated"],
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 12,
+        },
+      });
+
+      map.current?.addLayer({
+        id: "unclustered-point",
+        type: "circle",
+        source: "distributeurs",
+        filter: ["!", ["has", "point_count"]],
+        paint: {
+          "circle-color": "#11b4da",
+          "circle-radius": 4,
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "#fff",
         },
       });
     });
@@ -63,8 +93,40 @@ export async function getServerSideProps() {
   const d = await fetch(
     "https://www.data.gouv.fr/fr/datasets/r/d75f05c4-f8a5-49ee-81d2-1458232286fc"
   );
-  const data = await d.json();
+  const data: GeoJSON.FeatureCollection = await d.json();
+
+  // assume geojson is the input GEOJson with MultiPoint features
+
+  let outputGeoJson: GeoJSON.FeatureCollection = {
+    type: "FeatureCollection",
+    features: [],
+  };
+
+  data.features.forEach((feature) => {
+    if (feature.geometry.type === "MultiPoint") {
+      // select only the first coordinate of the MultiPoint
+      const [lon, lat] = feature.geometry.coordinates[0];
+
+      // create a new Point feature with the first coordinate
+      const pointFeature: GeoJSON.Feature = {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [lon, lat],
+        },
+        properties: feature.properties,
+      };
+
+      // add the new Point feature to the output GeoJson
+      outputGeoJson.features.push(pointFeature);
+    } else {
+      // add non-MultiPoint features as-is
+      outputGeoJson.features.push(feature);
+    }
+  });
+
+  // outputGeoJson now contains the converted GEOJson with Point features
 
   // Pass data to the page via props
-  return { props: { data } };
+  return { props: { data: outputGeoJson } };
 }
