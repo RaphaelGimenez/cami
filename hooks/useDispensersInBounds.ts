c35ddfc2269e5f5326b0defdc9b29e04e91a21f6
Raphael <contact@raphaelgimenez.fr>
2023-02-20 17:03:48 +0100

    ref: move extract function in useCallback

--- hooks/useDispensersInBounds.ts
+++ hooks/useDispensersInBounds.ts
@@ -1,5 +1,5 @@
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useState } from "react";
 import { LngLatBounds } from "mapbox-gl";
 
@@ -11,54 +11,55 @@ const useDispenersInBounds = (bounds: LngLatBounds | undefined) => {
   const [dispensers, setDispensers] = useState<GeoJSON.FeatureCollection>();
   const supabase = useSupabaseClient();
 
+  const getDispensers = useCallback(async () => {
+    const { data, error } = await supabase.rpc("dispensers_in_view", {
+      min_lat: bounds!.getSouth(),
+      min_long: bounds!.getWest(),
+      max_lat: bounds!.getNorth(),
+      max_long: bounds!.getEast(),
+    });
+
+    const regex = /\(([^)]+)\)/;
+
+    const outputGeoJson: GeoJSON.FeatureCollection = {
+      type: "FeatureCollection",
+      features:
+        (data as unknown as Dispenser[])?.map((d) => {
+          const matches = d.location.match(regex);
+          if (!matches) {
+            return {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "Point",
+                coordinates: [],
+              },
+            };
+          }
+          const coordinates = matches[1].split(" ");
+
+          return {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "Point",
+              coordinates: [
+                parseFloat(coordinates[0]),
+                parseFloat(coordinates[1]),
+              ],
+            },
+          };
+        }) || [],
+    };
+
+    setDispensers(outputGeoJson);
+  }, [bounds, supabase]);
+
   useEffect(() => {
     if (bounds) {
-      const getDispensers = async () => {
-        const { data, error } = await supabase.rpc("dispensers_in_view", {
-          min_lat: bounds.getSouth(),
-          min_long: bounds.getWest(),
-          max_lat: bounds.getNorth(),
-          max_long: bounds.getEast(),
-        });
-
-        const regex = /\(([^)]+)\)/;
-
-        const outputGeoJson: GeoJSON.FeatureCollection = {
-          type: "FeatureCollection",
-          features:
-            (data as unknown as Dispenser[])?.map((d) => {
-              const matches = d.location.match(regex);
-              if (!matches) {
-                return {
-                  type: "Feature",
-                  properties: {},
-                  geometry: {
-                    type: "Point",
-                    coordinates: [],
-                  },
-                };
-              }
-              const coordinates = matches[1].split(" ");
-
-              return {
-                type: "Feature",
-                properties: {},
-                geometry: {
-                  type: "Point",
-                  coordinates: [
-                    parseFloat(coordinates[0]),
-                    parseFloat(coordinates[1]),
-                  ],
-                },
-              };
-            }) || [],
-        };
-
-        setDispensers(outputGeoJson);
-      };
       getDispensers();
     }
-  }, [bounds, supabase]);
+  }, [getDispensers, bounds]);
 
   return dispensers;
 };
